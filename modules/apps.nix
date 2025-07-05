ae6a4e48080aa52ae9d6084981bea6cc654bbb39
jp-roisin <jeanpaul.roisin@protonmail.com>
2025-07-05 18:25:05 +0200

    Init with minimal nix-darwin-kickstarter

---
+++ modules/apps.nix
@@ -0,0 +1,50 @@
+{ pkgs, ...}: {
+
+  ##########################################################################
+  #
+  #  Install all apps and packages here.
+  #
+  #  NOTE: Your can find all available options in:
+  #    https://daiderd.com/nix-darwin/manual/index.html
+  #
+  ##########################################################################
+
+  # Install packages from nix's official package repository.
+  #
+  # The packages installed here are available to all users, and are reproducible across machines, and are rollbackable.
+  # But on macOS, it's less stable than homebrew.
+  #
+  # Related Discussion: https://discourse.nixos.org/t/darwin-again/29331
+  environment.systemPackages = with pkgs; [
+    git
+    neovim
+  ];
+
+  # The apps installed by homebrew are not managed by nix, and not reproducible!
+  # But on macOS, homebrew has a much larger selection of apps than nixpkgs, especially for GUI apps!
+  homebrew = {
+    enable = true;
+
+    onActivation = {
+      autoUpdate = false;
+      # 'zap': uninstalls all formulae(and related files) not listed here.
+      # cleanup = "zap";
+    };
+
+    # taps = [
+    #   "homebrew/services"
+    # ];
+
+    # `brew install`
+    brews = [
+      # "aria2"  # download tool
+    ];
+
+    # `brew install --cask`
+    casks = [
+     # "firefox"
+     "alacritty"
+      # "google-chrome"
+    ];
+  };
+}
