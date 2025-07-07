b2febbe652e351a4ee348d73fe69c8021210d762
jp-roisin <jeanpaul.roisin@protonmail.com>
2025-07-07 12:53:44 +0200

    Add golang path

--- home/shell.nix
+++ home/shell.nix
@@ -3,7 +3,7 @@
     enable = true;
     enableCompletion = true;
     initExtra = ''
-      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+      export PATH="$PATH:/usr/local/go/bin:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
     '';
 
     oh-my-zsh = {
