fc8961f588485c07912988c660200427b75fdb65
jp-roisin <jeanpaul.roisin@protonmail.com>
2025-08-13 12:48:08 +0200

    Disable two fingers swipe between pages

--- modules/system.nix
+++ modules/system.nix
@@ -74,6 +74,10 @@
         NSAutomaticSpellingCorrectionEnabled = false; # disable auto spelling correction(自动拼写检查)
         NSNavPanelExpandedStateForSaveMode = true; # expand save panel by default(保存文件时的路径选择/文件名输入页)
         NSNavPanelExpandedStateForSaveMode2 = true;
+
+        # Disable two fingers swipe between pages (browser navigation)
+        AppleEnableMouseSwipeNavigateWithScrolls = false;
+        AppleEnableSwipeNavigateWithScrolls = false;
       };
 
       # Customize settings that not supported by nix-darwin directly
