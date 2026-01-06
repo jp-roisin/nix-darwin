5e37ac26b78916efb85e5d31d439f89df68b3dcc
jp-roisin <jeanpaul.roisin@protonmail.com>
2026-01-06 13:37:34 +0100

    Open aerospace and docker on boot

--- modules/system.nix
+++ modules/system.nix
@@ -159,6 +159,35 @@
   # Add ability to used TouchID for sudo authentication
   security.pam.services.sudo_local.touchIdAuth = true;
 
+  # Auto-start applications on login
+  launchd.user.agents = {
+    aerospace = {
+      serviceConfig = {
+        ProgramArguments = [ 
+          "/usr/bin/open"
+          "-a"
+          "/Applications/AeroSpace.app"
+        ];
+        RunAtLoad = true;
+        StandardOutPath = "/tmp/aerospace.log";
+        StandardErrorPath = "/tmp/aerospace.err.log";
+      };
+    };
+
+    docker-desktop = {
+      serviceConfig = {
+        ProgramArguments = [ 
+          "/usr/bin/open"
+          "-a"
+          "/Applications/Docker.app"
+        ];
+        RunAtLoad = true;
+        StandardOutPath = "/tmp/docker-desktop.log";
+        StandardErrorPath = "/tmp/docker-desktop.err.log";
+      };
+    };
+  };
+
   # Create /etc/zshrc that loads the nix-darwin environment.
   # this is required if you want to use darwin's default shell - zsh
   programs.zsh.enable = true;
